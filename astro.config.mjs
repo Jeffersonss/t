b7b359aa2a2da8ddf58d4f95ad3cd08a32ce5fb3
Jefferson Sales dos Santos <Jeffersonss@hotmail.com>
2022-11-04 16:50:45 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+build: {
+    // Exemplo: Gera `pagina.html` ao invés de `pagina/index.html` durante a build.
+    format: 'directory'
+  }
